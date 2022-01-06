4de781ad608dbc5a95ef2f6a8276704a7638729e
camilavargasp <cvargas@bren.ucsb.edu>
2022-01-06 11:28:27 -0800

    template info

--- edi.1045/run_EMLassemblyline_for_edi.1045.R
+++ edi.1045/run_EMLassemblyline_for_edi.1045.R
@@ -32,7 +32,7 @@ EMLassemblyline::template_core_metadata(
 EMLassemblyline::template_table_attributes(
   path = path_templates,
   data.path = path_data,
-  data.table = c("2016_and_2019_soil_wood_samples_loc.csv"))
+  data.table = "soil_wood_samples_loc_info_2016_2019.csv")
 
 # Create categorical variables template (required when attributes templates
 # contains variables with a "categorical" class)
@@ -44,29 +44,29 @@ EMLassemblyline::template_categorical_variables(
 # Create geographic coverage (required when more than one geographic location
 # is to be reported in the metadata).
 
-EMLassemblyline::template_geographic_coverage(
-  path = path_templates, 
-  data.path = path_data, 
-  data.table = "", 
-  lat.col = "",
-  lon.col = "",
-  site.col = "")
+# EMLassemblyline::template_geographic_coverage(
+#   path = path_templates, 
+#   data.path = path_data, 
+#   data.table = "", 
+#   lat.col = "",
+#   lon.col = "",
+#   site.col = "")
 
 # Create taxonomic coverage template (Not-required. Use this to report 
 # taxonomic entities in the metadata)
 
-remotes::install_github("EDIorg/taxonomyCleanr")
-library(taxonomyCleanr)
-
-taxonomyCleanr::view_taxa_authorities()
-
-EMLassemblyline::template_taxonomic_coverage(
-  path = path_templates, 
-  data.path = path_data,
-  taxa.table = "",
-  taxa.col = "",
-  taxa.name.type = "",
-  taxa.authority = 3)
+# remotes::install_github("EDIorg/taxonomyCleanr")
+# library(taxonomyCleanr)
+# 
+# taxonomyCleanr::view_taxa_authorities()
+# 
+# EMLassemblyline::template_taxonomic_coverage(
+#   path = path_templates, 
+#   data.path = path_data,
+#   taxa.table = "",
+#   taxa.col = "",
+#   taxa.name.type = "",
+#   taxa.authority = 3)
 
 # Make EML from metadata templates --------------------------------------------
 
@@ -77,17 +77,17 @@ EMLassemblyline::make_eml(
   path = path_templates,
   data.path = path_data,
   eml.path = path_eml, 
-  dataset.title = "", 
-  temporal.coverage = c("YYYY-MM-DD", "YYYY-MM-DD"), 
-  geographic.description = "", 
-  geographic.coordinates = c("N", "E", "S", "W"), 
-  maintenance.description = "", 
-  data.table = c(""), 
-  data.table.name = c(""),
-  data.table.description = c(""),
-  other.entity = c(""),
-  other.entity.name = c(""),
-  other.entity.description = c(""),
-  user.id = "",
-  user.domain = "", 
-  package.id = "")
+  dataset.title = "Palmyra Atoll soil and/or wood density sampling locations used in the carbon storage analysis", 
+  temporal.coverage = c("2016", "2019"), 
+  geographic.description = "Palmyra Atoll", 
+  geographic.coordinates = c("5.88333", "-162.04300", "5.87100", "-162.08300"), 
+  maintenance.description = "completed", 
+  data.table = c("soil_wood_samples_loc_info_2016_2019.csv"), 
+  data.table.name = c("Palmyra Atoll Soil and Wood Sample attributes  2016 and 2019"),
+  data.table.description = c("Additional information of trees and soil samples"),
+  other.entity = c("soil_wood_samples_location_palmyra_2016_2019.zip"),
+  other.entity.name = c("Shapefiles with geospatil data"),
+  other.entity.description = c("zip file with all necessary files to read in the location of the sampling for this project"),
+  user.id = "palmyra_priject",
+  user.domain = "EDI", 
+  package.id = "1045.1")
